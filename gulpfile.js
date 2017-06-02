d1131b53d001ddaf02c38a404a20af1f6b971aa7
Pierre <fritschpierre@gmail.com>
2017-06-02 10:12:07 +0200

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,46 @@
 // Requirements
-// ------------------------
-var gulp = require('gulp'),
+// -----------------------------------------------------------------------------
+const gulp = require('gulp')
 
-  // Utils
-  rename = require('gulp-rename'),
-  header = require('gulp-header'),
-  inject = require('gulp-inject'),
-  concat = require('gulp-concat'),
-  
-  // Template
-  minifyHTML = require('gulp-minify-html'),
-  wiredep = require('wiredep').stream,
-  svgstore = require('gulp-svgstore'),
-  
-  // Images
-  imagemin = require('gulp-imagemin'),
-  pngquant = require('imagemin-pngquant'),
-  
-  // Scripts
-  jshint = require('gulp-jshint'),
-  stylish = require('jshint-stylish'),
-  uglify = require('gulp-uglify'),
+// Utils
+const rename = require('gulp-rename')
+const header = require('gulp-header')
+const inject = require('gulp-inject')
+const concat = require('gulp-concat')
+const clean = require('gulp-clean')
 
-  // Styles
-  sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer'),
-  sourcemaps = require('gulp-sourcemaps'),
-  minifyCSS = require('gulp-minify-css'),
-  
-  // Doc
-  hologram = require('gulp-hologram'),
-  
-  // Server
-  browserSync = require('browser-sync'),
+// Template
+const minifyHTML = require('gulp-minify-html')
+const wiredep = require('wiredep').stream
+const svgstore = require('gulp-svgstore')
 
-  // SEO
-  sitemap = require('gulp-sitemap');
+// Images
+const imagemin = require('gulp-imagemin')
+const pngquant = require('imagemin-pngquant')
 
+// Scripts
+const jshint = require('gulp-jshint')
+const stylish = require('jshint-stylish')
+const uglify = require('gulp-uglify')
+
+// Styles
+const sass = require('gulp-sass')
+const plumber = require('gulp-plumber')
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS = require('gulp-clean-css')
+const sourcemaps = require('gulp-sourcemaps')
+
+// Doc
+const hologram = require('gulp-hologram')
+
+// Server
+const browserSync = require('browser-sync')
+
+// SEO
+const sitemap = require('gulp-sitemap')
 
 // Settings
-// ------------------------
+// -----------------------------------------------------------------------------
 var banner = [
   '/*!\n' +
   ' * <%= package.name %>\n' +
@@ -50,39 +51,52 @@ var banner = [
   ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
   ' */',
   '\n'
-].join('');
+].join('')
 
-var src = './src/',
-  dist = './app/',
-  package = require('./package.json'),
-  reload = browserSync.reload;
-
+const src = './src/'
+const dist = './app/'
+const siteUrl = '<%= package.url %>'
+const packages = require('./package.json')
+const reload = browserSync.reload
 
 // Gulp Tasks
-// ------------------------
-
+// -----------------------------------------------------------------------------
 
 // COPY
 // Copy extra files like .htaccess, robots.txt
 gulp.task('copy', function () {
   return gulp.src(['./.htaccess', './robots.txt'])
-    .pipe(gulp.dest(dist));
-});
+  .pipe(gulp.dest(dist))
+})
 
+// Copy fonts
+gulp.task('fonts', function () {
+  return gulp.src(src + 'assets/fonts/*')
+    .pipe(gulp.dest(dist + 'assets/fonts/'))
+})
+
+// Copy docs
+gulp.task('docs', function () {
+  return gulp.src(src + 'assets/*')
+    .pipe(gulp.dest(dist + 'assets/'))
+})
 
 // TEMPLATE
 // Bower css and scripts inject +  SVG Sprite inject
 gulp.task('template', function () {
-
   var svgs = gulp
     .src(src + 'assets/icons/*.svg')
-    .pipe(svgstore({ inlineSvg: true }));
-
+    .pipe(imagemin({
+      svgoPlugins: [{
+        removeViewBox: false
+      }]
+    }))
+    .pipe(rename({prefix: 'icon-'}))
+    .pipe(svgstore({ inlineSvg: true }))
   function fileContents (filePath, file) {
-    return file.contents.toString();
+    return file.contents.toString()
   }
-
-  return gulp.src(src + '*.html')
+  return gulp.src(src + '**/*.html')
     .pipe(wiredep({
       includeSelf: true
     }))
@@ -91,11 +105,10 @@ gulp.task('template', function () {
       conditionals: true,
       spare: true
     }))
-    .pipe(gulp.dest(dist));
-});
+    .pipe(gulp.dest(dist))
+})
 
-gulp.task('template-watch', ['template'], reload);
-
+gulp.task('template-watch', ['template'], reload)
 
 // IMAGES
 // Optimization
@@ -111,9 +124,8 @@ gulp.task('images', function () {
     .pipe(gulp.dest(dist + 'assets/images/'))
     .pipe(reload({
       stream: true
-    }));
-});
-
+    }))
+})
 
 // SCRIPTS
 // JSHint, Uglify
@@ -126,17 +138,18 @@ gulp.task('scripts', function () {
     .pipe(gulp.dest(dist + 'scripts'))
     .pipe(reload({
       stream: true
-    }));
-});
+    }))
+})
+
 // Bower components main scripts files
-gulp.task('vendors', function() {
-  var vendorsJS = require('wiredep')().js;
+gulp.task('vendors', function () {
+  var vendorsJS = require('wiredep')().js
+  // vendorsJS.push('src/be.js/be.js')
   return gulp.src(vendorsJS)
     .pipe(concat('vendors.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest(dist+'scripts'));
-});
-
+    .pipe(gulp.dest(dist + 'scripts'))
+})
 
 // STYLES
 // LibSass, Minified
@@ -144,6 +157,7 @@ gulp.task('styles', function () {
   return gulp.src(src + 'styles/{,*/}*.{scss,sass}')
     .pipe(sourcemaps.init())
     .pipe(wiredep())
+    .pipe(plumber())
     .pipe(sass({
       errLogToConsole: true
     }))
@@ -151,20 +165,19 @@ gulp.task('styles', function () {
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(minifyCSS())
+    .pipe(cleanCSS({compatibility: 'ie9'}))
     .pipe(rename({
       suffix: '.min'
     }))
     .pipe(header(banner, {
-      package: package
+      package: packages
     }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(dist + 'styles'))
     .pipe(reload({
       stream: true
-    }));
-});
-
+    }))
+})
 
 // DOC
 // Hologram: http://www.wearecube.ch/maintaining-living-style-guides-with-hologram/
@@ -173,21 +186,28 @@ gulp.task('doc', function () {
     .pipe(hologram())
     .pipe(reload({
       stream: true
-    }));
-});
+    }))
+})
 
 // SEO
 // Generate a Sitemap
 gulp.task('sitemap', function () {
-  return gulp.src(dist+'/*.html')
+  return gulp.src(dist + '/*.html')
     .pipe(sitemap({
-      siteUrl: 'http://www.wearecube.ch/frontendcubie/'
+      siteUrl: siteUrl
     }))
-    .pipe(gulp.dest(dist));
-});
+    .pipe(gulp.dest(dist))
+})
+
+// CLEAN
+// Generate a Sitemap
+gulp.task('clean', function () {
+  return gulp.src(dist)
+    .pipe(clean())
+})
 
 // BUILD
-gulp.task('build', ['copy', 'vendors', 'template', 'images', 'scripts', 'styles'], reload);
+gulp.task('build', ['copy', 'docs', 'fonts', 'vendors', 'template', 'images', 'scripts', 'styles'], reload)
 
 // SERVER
 // Browser Sync (wait build task to be done)
@@ -196,19 +216,17 @@ gulp.task('serve', ['build'], function () {
     notify: false,
     server: {
       baseDir: dist,
-      routes: {
-        "/bower_components": "bower_components"
-      }
+      routes: { '/bower_components': 'bower_components' }
     }
-  });
-  gulp.watch(src + '**/*.{html,json,svg}', ['template-watch']);
-  gulp.watch(src + 'scripts/*.js', ['scripts']);
-  gulp.watch(src + 'assets/images/*', ['images']);
-  gulp.watch(src + 'styles/{,*/}*.{scss,sass}', ['styles', 'doc']);
-  gulp.watch(src + 'styles/styleguide.md', ['doc']);
-});
+  })
+  gulp.watch(src + '**/*.{html,json,svg}', ['template-watch', 'copy'])
+  gulp.watch(src + 'scripts/*.js', ['scripts'])
+  gulp.watch(src + 'assets/images/*', ['images'])
+  gulp.watch(src + 'styles/{,*/}*.{scss,sass}', ['styles', 'doc'])
+  gulp.watch(src + 'styles/styleguide.md', ['doc'])
+})
 
 // Gulp Default Task
-// ------------------------
+// -----------------------------------------------------------------------------
 // Having watch within the task ensures that 'sass' has already ran before watching
-gulp.task('default', ['build', 'doc', 'sitemap', 'serve']);
+gulp.task('default', ['build', 'doc', 'sitemap', 'serve'])
